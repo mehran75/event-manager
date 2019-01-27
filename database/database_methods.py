16ae38c5760dd26a24d3a64808662daac3daebee
mehran <mehranrafiee5@gmail.com>
2019-01-27 21:02:32 +0330

    database models and methods

---
+++ database/database_methods.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+from django.contrib import admin
+
+salt = '12@{}E#E}{'
+hash_algorithm = 'default'
+
+
+def get_hashed_password(password):
+    return make_password(password, salt, hash_algorithm)
+
+
+def check_user_exist(mail, raw_password):
+    """
+    validate user
+    :param mail: str
+    :param raw_password: plain text
+    :return: User model if validate otherwise, None
+    """
+    u = User(email=mail, password=make_password(raw_password, salt, hash_algorithm))
+
+    if u.objects.count() == 0:
+        return None
+    else:
+        return u.objects
+
+
+def register_new_user(fields_dic, is_admin=False):
+    """
+    register new User
+    :param is_admin: only if Admin is creating a user
+    :param fields_dic: dictionary
+    :return: User object
+    """
+
+    # check if required fileds exists in dictionary
+    if 'mail' and 'password' and 'first_name' and 'last_name' not in fields_dic.keys():
+        return None
+    #
+    # user = User(email=fields_dic['mail'], username=fields_dic['mail'],
+    #             password=get_hashed_password(fields_dic['password']),
+    #             first_name=fields_dic['first_name'], last_name=fields_dic['last_name'])
+
+    user = User.objects.create_user(fields_dic['mail'], email=fields_dic['mail'],
+                                    password=get_hashed_password(fields_dic['password']),
+                                    first_name=fields_dic['first_name'], last_name=fields_dic['last_name'])
+
+    user.is_active = is_admin
+    user.is_staff = is_admin
+    user.date_joined = datetime.now()
+
+    if User.objects.filter(username=fields_dic['mail']).exists():
+        return None
+    user.save()
+
+    return user
---
+++ database/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    title = models.TextField(blank=False)
+
+    body = models.TextField()
+
+
+class Event(models.Model):
+    title = models.TextField(blank=False)
+    body = models.TextField()
+
+    date = timezone.now()
+
+    categoryId = models.ForeignKey(Category, on_delete=models.CASCADE, blank=False)
+
+
+# here is only answer to requests
+class Request(models.Model):
+    REQUEST_ACCEPT = 'accepted'
+    REQUEST_DENY = 'failed'
+    REQUEST_WAITING = 'waiting'
+
+    RESULTS = [
+        (REQUEST_DENY, 0),
+        (REQUEST_ACCEPT, 1),
+        (REQUEST_WAITING, 2)
+    ]
+
+    # sender is users
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    # receiver is the admin(s)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    date = timezone.now()
+    result = models.IntegerField(blank=False, choices=RESULTS)
+
+    # optional
+    text = models.TextField()
+
+
+class Resume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    path = models.TextField(blank=False)
+
+
+class Skill(models.Model):
+    title = models.TextField(blank=False)
+
+
+class Skills(models.Model):
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# u = User(user_type=User.ADMIN)
+#
+# u.mail = 'mehranrafiee5@gmail.com'
+# u.first_name = 'Mehran'
+# u.last_name = 'Rafiee'
+# u.set_password('1234')
+# u.save()
